8db200f668be553f71afce082bd6bc85aeac5d40
MarymelCadorna-UCM <marymelmontemayor@gmail.com>
2025-09-02 21:59:57 +0800

    Update FavoriteSong.java

--- Chapter_1/FavoriteSong.java
+++ Chapter_1/FavoriteSong.java
@@ -1,9 +1,8 @@
 public class FavoriteSong {
     public static void main(String[] args) {
-        System.out.println("Happiness");
-        System.out.println("Goodness of God");
-        System.out.println("You will be in my heart");
-        System.out.println("I like me better");
+        System.out.println("The feeling of choosing joy even in tough times");
+        System.out.println("The idea that God’s love and faithfulness never fail");
+        System.out.println("The reassurance that love protects and endures no matter the distance");
+        System.out.println("The joy of becoming your best self when you’re with someone you love");
     }
 }
-
