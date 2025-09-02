c65a4bce025aa640b083cf0685bb7788fe822799
MarymelCadorna-UCM <marymelmontemayor@gmail.com>
2025-09-02 20:06:25 +0800

    Update Comments.java

--- Chapter_1/Comments.java
+++ Chapter_1/Comments.java
@@ -1,1 +1,12 @@
+package OOP_1;
+
+public class Comments {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello, Java comments!");
+
+        System.out.println("This program demonstrates all 3 types of comments.");
+    }
+}
+
 
