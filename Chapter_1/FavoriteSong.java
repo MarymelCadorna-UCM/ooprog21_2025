d03b596b52bf5656b07b4bd52f1edaaecfd895ae
MarymelCadorna-UCM <marymelmontemayor@gmail.com>
2025-09-02 19:47:32 +0800

    Update FavoriteSong.java

--- Chapter_1/FavoriteSong.java
+++ Chapter_1/FavoriteSong.java
@@ -1,1 +1,11 @@
+package OOP_1;
+
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("Happiness");
+        System.out.println("Goodness of God");
+        System.out.println("You will be in my heart");
+        System.out.println("I like me better");
+    }
+}
 
