5a53730906a32ff6ff5428c029cfb49ecade273f
MarymelCadorna-UCM <marymelmontemayor@gmail.com>
2025-09-02 19:03:48 +0800

    Update TableAndChairs.java

--- Chapter_1/TableAndChairs.java
+++ Chapter_1/TableAndChairs.java
@@ -1,1 +1,17 @@
+public class TableAndChairs {
+    public static void main(String[] args) {
+        String[] pattern = {
+            "X                        X",
+            "X                        X",
+            "X      XXXXXXXXXXXX      X",
+            "XXXXX  X          X  XXXXX",
+            "X   X  X          X  X   X",
+            "X   X  X          X  X   X"
+        };
+
+        for (int i = 0; i < pattern.length; i++) {
+            System.out.println(pattern[i]);
+        }
+    }
+}
 
