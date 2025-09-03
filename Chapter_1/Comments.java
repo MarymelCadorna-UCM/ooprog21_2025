6b0a59dc37eb8a5b05eab2d61297012a0da9e03f
MarymelCadorna-UCM <marymelmontemayor@gmail.com>
2025-09-03 08:17:18 +0800

    Update Comments.java

--- Chapter_1/Comments.java
+++ Chapter_1/Comments.java
@@ -3,7 +3,6 @@ public class Comments {
     public static void main(String[] args) {
 
         System.out.println("Hello, this program shows the 3 different types of comments!");
-
         System.out.println("Single-line, multi-line, and documentation comments.");
     }
 }
