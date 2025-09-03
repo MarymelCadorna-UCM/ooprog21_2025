5534fd97c577ef78ee486f1d76619953899a3d79
MarymelCadorna-UCM <marymelmontemayor@gmail.com>
2025-09-03 08:50:15 +0800

    Update Comments.java

--- Chapter_1/Comments.java
+++ Chapter_1/Comments.java
@@ -1,8 +1,17 @@
-public class Comments {
+public class Comments { 
 
-    public static void main(String[] args) {
-
-        System.out.println("Hello, this program shows the 3 different types of comments!");
-        System.out.println("Single-line, multi-line, and documentation comments.");
+    public static void main(String[] args) { 
+        // 1. Single-line comment
+        System.out.println("This is a single-line comment example"); 
+        /* 
+        * 2. Multi-line comment 
+        * This type of comment can span * multiple lines. 
+        */ 
+        System.out.println("This is a multi-line comment example"); 
+        /** 
+        * 3. Documentation comment 
+        * Used to describe classes, methods, and variables. * Can be processed by Javadoc tool to generate documentation. 
+        */ 
+        System.out.println("This is a documentation comment example");
     }
 }
