050e197c03b6b43f215dfab71bc0e31aca83deb3
MarymelCadorna-UCM <marymelmontemayor@gmail.com>
2025-09-03 08:17:08 +0800

    Update Comments.java

--- Chapter_1/Comments.java
+++ Chapter_1/Comments.java
@@ -2,21 +2,8 @@ public class Comments {
 
     public static void main(String[] args) {
 
-        // Display message to the user
-        // This single-line comment explains the purpose of the next line
         System.out.println("Hello, this program shows the 3 different types of comments!");
 
-        /*
-         * The following is a multi-line comment.
-         * It is used for more detailed explanations or notes.
-         * This block could span several lines to describe a complex process.
-         */
         System.out.println("Single-line, multi-line, and documentation comments.");
-
-        /**
-         * This is a documentation comment.
-         * Documentation comments are special as they are used to generate Java API documentation.
-         * In larger projects, these help developers understand code and provide details about methods and classes.
-         */
     }
 }
