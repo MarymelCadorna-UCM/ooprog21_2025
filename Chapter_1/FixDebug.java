e41df838af6a7926aa3a48808b374c094b694837
MarymelCadorna-UCM <marymelmontemayor@gmail.com>
2025-09-08 22:41:00 +0800

    Update FixDebug.java

--- Chapter_1/FixDebug.java
+++ Chapter_1/FixDebug.java
@@ -1,1 +1,22 @@
+public class FixDebug One
+{
+  /* This program displays a greeting */
+  public static void Main(String args)
+  {
+    Systm.out.println("Hello World!")
+  }
 
+/*
+List down the spotted bugs below:
+
+e.g.
+- // wrong syntax for access modifier "publlic" instead of "public"
+
+Bugs:
+1. // Wrong class name: "FixDebug One" should be "FixDebugOne"
+2. // Wrong method header: "Main" should be "main"
+3. // Wrong parameter: "String args" should be "String[] args"
+4. // Misspelled class: "Systm" should be "System"
+5. // Missing semicolon: after System.out.println("Hello World!");
+6. // Missing closing brace: one extra } is needed at the end of the file to close the class.
+*/
