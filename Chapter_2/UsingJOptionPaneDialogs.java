766d6b96069fddc9d9e9a49664b8d4c82df316f0
MarymelCadorna-UCM <marymelmontemayor@gmail.com>
2025-09-22 08:00:46 +0800

    Create UsingJOptionPaneDialogs.java

---
+++ Chapter_2/UsingJOptionPaneDialogs.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+public class UsingOptionPaneDialogs {
+    public static void main(String[] args) {
+        while (true) {
+            String name = JOptionPane.showInputDialog(null, "What is your name?", "Input", JOptionPane.QUESTION_MESSAGE);
+
+            if (name == null || name.trim().isEmpty()) {
+                break;
+            }
+
+            int response = JOptionPane.showConfirmDialog(null, "Do you want to proceed?", "Confirmation", JOptionPane.YES_NO_OPTION);
+
+            if (response == JOptionPane.YES_OPTION) {
+                JOptionPane.showMessageDialog(null, "Hello, " + name + "!", "Message", JOptionPane.INFORMATION_MESSAGE);
+                break;
+            }
+        }
+    }
+}
+
