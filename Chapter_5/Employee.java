1f51e9cd902b85bff3f3e4eca3af5f0b5143e774
MarymelCadorna-UCM <marymelmontemayor@gmail.com>
2025-10-08 12:25:38 +0800

    Create Employee.java

---
+++ Chapter_5/Employee.java
@@ -0,0 +1,26 @@
+public class Employee {
+    private int employeeNumber;
+    private double payRate;
+    public static final int MAX_EMPLOYEE_NUMBER = 9999;
+    public static final double MAX_PAY_RATE = 60.0;
+    public static final double OVERTIME_MULTIPLIER = 1.5;
+
+    public Employee(int employeeNumber, double payRate) {
+        this.employeeNumber = employeeNumber;
+        this.payRate = payRate;
+    }
+
+    public double calculateRegularPay(double hoursWorked) {
+        double regularHours = Math.min(40, hoursWorked);
+        return regularHours * payRate;
+    }
+
+    public double calculateOvertimePay(double hoursWorked) {
+        double overtimeHours = Math.max(0, hoursWorked - 40);
+        return overtimeHours * payRate * OVERTIME_MULTIPLIER;
+    }
+
+    public int getEmployeeNumber() {
+        return employeeNumber;
+    }
+}
